744b40c8e3d6c12110683c84de4dbe50be4eae42
joindevopscloud <info@joindevops.com>
2026-05-21 08:34:14 +0530

    hello world

---
+++ 04-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Please enter your username::"
+read USER_NAME # here USER_NAME is variable, whatever you enter in terminal stores in this variable
+
+echo "Username is $USER_NAME"
