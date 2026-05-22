d361f6bb1bc6fcfb4746631a4d8e80184eb8b9f5
joindevopscloud <info@joindevops.com>
2026-05-22 07:51:11 +0530

    hello world

--- 07-data-types.sh
+++ 07-data-types.sh
@@ -10,3 +10,6 @@ echo "Sum is: $SUM"
 # Array
 MOVIES=("RRR" "Varanasi" "Pushpa") # index always starts from 0
 echo "Movies are: ${MOVIES[@]}"
+echo "First movie is: ${MOVIES[0]}"
+echo "Second movie is: ${MOVIES[1]}"
+echo "Third movie is: ${MOVIES[2]}"
