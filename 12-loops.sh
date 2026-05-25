3ce42e37357f4649858113b4964286c30b14fe4c
joindevopscloud <info@joindevops.com>
2026-05-25 07:35:21 +0530

    api doc added

---
+++ 12-loops.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+USERID=$(id -u)
+OGS_DIR=/var/log/shell-script
+LOGS_FILE="$LOGS_DIR/$0.log"
+
+# Check root access or not
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script with root access"
+    exit 1
+fi
+
+# first arg -> what are you trying to install
+# second arg -> exit code
+VALIDATE(){
+    if [ $2 -ne 0 ]; then
+        echo "Installing $1 is ... FAILED"
+        exit 1
+    else
+        echo "Installing $1 is ... SUCCESS"
+    fi
+}
+
+
+for package in $@
+do
+    echo "Installing $package"
+done
