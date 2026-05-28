c157d07a2721dc1b4a251d6cb854475ed0376fef
joindevopscloud <info@joindevops.com>
2026-05-28 08:37:02 +0530

    api doc added

--- 19-logs-cleanup.sh
+++ 19-logs-cleanup.sh
@@ -13,3 +13,19 @@ if [ ! -d $SOURCE_DIR ]; then
     echo "ERROR: Directory: $SOURCE_DIR does not exist"
     exit 1
 fi
+
+echo "Scanning $SOURCE_DIR for log files older than 14 days"
+FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS)
+
+if [ -z $FILES ]; then
+    echo "No log files older than 14 days found"
+    exit 0
+fi
+
+while IFS= read -r FILE
+do
+    echo "File to be deleted: $FILE"
+done <<< "$FILES"
+
+# < -> for file
+# <<< -> variable
