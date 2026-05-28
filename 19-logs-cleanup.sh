5721c96287defa768a76969837b7f40384ee3625
joindevopscloud <info@joindevops.com>
2026-05-28 08:32:10 +0530

    api doc added

--- 19-logs-cleanup.sh
+++ 19-logs-cleanup.sh
@@ -5,6 +5,11 @@ DAYS=${2:-14} # default 14 days
 
 if [ -z $SOURCE_DIR ]; then
     echo "ERROR:: Missing parameters"
-    echo "USAGE: $0 <source-dir> [days(optional default to 14)]"
+    echo "USAGE:: $0 <source-dir> [days(optional default to 14)]"
+    exit 1
+fi
+
+if [ ! -d $SOURCE_DIR ]; then
+    echo "ERROR: Directory: $SOURCE_DIR does not exist"
     exit 1
 fi
